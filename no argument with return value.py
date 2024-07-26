b9fe6ba6963347a763b4fc16d3d1cf53f2a48d40
ramakoti2005 <ramakotireddy2005@gmail.com>
2024-07-26 14:58:02 +0530

    Add files via upload

---
+++ no argument with return value.py
@@ -0,0 +1,7 @@
+def add():
+    n1=int(input("Enter the Number 1:"))
+    n2=int(input("Enter the Number 2:"))
+    return (n1+n2)
+
+#Function Call
+print("Addition is {}".format(add()))
---
+++ with argument with return value.py
@@ -0,0 +1,8 @@
+def add(n1,n2):#n1 & n2 both are Formal Arguments
+    return (n1+n2)
+
+#Function Call
+a=int(input("Enter the Number 1:"))
+b=int(input("Enter the Number 2:"))
+print("Addition is {}".format(add(a,b)))
+#a & b both are Actual Arguments
